3e8973d9996e982035432550677544872f9560b3
Vanshaj-HjInfotech <Vanshaj.hjintern@hotmail.com>
2025-09-22 11:29:02 +0530

    changing the name of strorage

--- terraform_VM/main.tf
+++ terraform_VM/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-test-ci-003"
-    storage_account_name = "strdatablob001"
+    storage_account_name = "strblobdataci001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     
