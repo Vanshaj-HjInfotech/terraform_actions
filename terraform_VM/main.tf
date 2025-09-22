d9107671f2af56e43d1a337f5ee0dd6bb683df49
Vanshaj-HjInfotech <Vanshaj.hjintern@hotmail.com>
2025-09-22 10:38:37 +0530

    adding backend

--- terraform_VM/main.tf
+++ terraform_VM/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-test-ci-003"
+    storage_account_name = "strdatablob001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    
+  }
 }
 
 
